d149de394d8e4dfd5bbba41861a82e1a99a0cc32
Pedro Sabas <pither111213.p@gmail.com>
2022-03-05 17:10:40 -0600

    Se implemento @CrossOrigin al controlador

--- src/main/java/com/sabas/student_apirest/student/controller/StudentController.java
+++ src/main/java/com/sabas/student_apirest/student/controller/StudentController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,6 +23,7 @@ import com.sabas.student_apirest.student.service.StudentService;
 
 @RestController
 @RequestMapping("/api/students")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST})
 public class StudentController {
 	
 	private StudentService studentService;
